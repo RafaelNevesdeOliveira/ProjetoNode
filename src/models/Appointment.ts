f0ac783178fa69e862a735485075acb54994edf8
Rafael Neves de Oliveira <rafaelneves652@gmail.com>
2020-12-12 14:23:59 -0300

    Added repositories and services

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -7,7 +7,8 @@ class Appointment {
 
     date: Date;
 
-    constructor(provider: string, date: Date){
+    // criar variaveis mas o Omit vai servir para apenas o ID nao ser criado
+    constructor({provider, date,}: Omit<Appointment, 'id'>) {
         this.id = uuid();
         this.provider = provider;
         this.date = date
--- src/routes/appointmentsRoutes.ts
+++ src/routes/appointmentsRoutes.ts
@@ -1,7 +1,8 @@
-import { request, response, Router } from 'express';
-import { startOfHour, parseISO} from 'date-fns'
+import { Router } from 'express';
+import { parseISO} from 'date-fns';
 
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,24 +14,22 @@ appointmentsRouter.get('/', ( request, response) => {
 })
 
 appointmentsRouter.post('/', (request, response) => {
+  try{
     const { provider, date } = request.body;
 
-    const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-        parsedDate,
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
     );
 
-    if(findAppointmentInSameDate){
-        return response
-        .status(400)
-        .json({message:"This appointment already booked" })
-    }
-
-    const appointment = appointmentsRepository.create(provider, parsedDate)
+    const appointment = createAppointment.execute({date: parsedDate, provider});
 
     return response.json(appointment)
-})
+  } catch (err){
+    return response.status(400).json({error: err.message})
+  }
+});
 
 export default appointmentsRouter;
 

b0f1ffc654a98436deff31e48497f20764229303
Rafael Neves de Oliveira <rafaelneves652@gmail.com>
2020-12-19 10:56:02 -0300

    Data Bank implementation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,18 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
-@Entity('appointments')
-class Appointment {
+
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column() // passar sem nada eh igual passar varchar
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')// caso nao use postgres, talvez não seja possível usar Time Zone
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -18,4 +22,4 @@ class Appointment {
 
 }
 
-export default Appointment;
+export default User;
